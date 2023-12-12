36bd5f1b01fc743b10100cc626cd527fb621ec3c
Jasdev2910 <jasdevvirdi987@gmail.com>
2023-12-12 21:15:38 +0530

    week one assignment done

--- assignments-master/01-js/hard/calculator.js
+++ assignments-master/01-js/hard/calculator.js
@@ -16,6 +16,55 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  add(number) {
+    this.result += number;
+  }
+
+  subtract(number) {
+    this.result -= number;
+  }
+
+  multiply(number) {
+    this.result *= number;
+  }
+
+  divide(number) {
+    if (number === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    this.result /= number;
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(expression) {
+    expression = expression.replace(/\s+/g, " ").trim();
+
+    if (!/^[\d\s\+\-\*\/\(\).]+$/.test(expression)) {
+      throw new Error("Invalid characters in the string");
+    }
+
+    if (expression.includes("/ 0")) {
+      throw new Error("Cannot divide by zero");
+    }
+
+    try {
+      this.result = parseFloat(eval(expression).toFixed(10));
+    } catch (error) {
+      throw new Error("Invalid Expression");
+    }
+  }
+}
 
 module.exports = Calculator;
