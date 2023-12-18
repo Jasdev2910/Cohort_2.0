efa0e632e0282ee98f0cd55ec8a53bd5d71b6307
Jasdev2910 <jasdevvirdi987@gmail.com>
2023-12-18 23:43:26 +0530

    week 2 easy done

---
+++ assignments-master/week-2/01-async-js/easy/4-write-to-file.js
@@ -0,0 +1,17 @@
+// ## Write to a file
+// Using the fs library again, try to write to the contents of a file.
+// You can use the fs library to as a black box, the goal is to understand async tasks.
+
+const fs = require("fs");
+
+fs.writeFile("a.txt", "this is some modification", function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("file updated");
+  }
+});
+
+fs.readFile("a.txt", "utf-8", function (err, data) {
+  console.log(data);
+});
