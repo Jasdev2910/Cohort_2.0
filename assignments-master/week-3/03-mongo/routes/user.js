49e9931f6e14bab4253ca03e2c7e545abbd80525
Jasdev2910 <jasdevvirdi987@gmail.com>
2024-01-05 13:37:33 +0530

    week 3 assignment 03-mongo

--- assignments-master/week-3/03-mongo/routes/user.js
+++ assignments-master/week-3/03-mongo/routes/user.js
@@ -1,22 +1,72 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
 
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post("/signup", async (req, res) => {
+  // Implement user signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+
+  await User.create({
+    username,
+    password,
+  });
+
+  res.json({
+    message: "User created successfully",
+  });
 });
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+router.get("/courses", async (req, res) => {
+  // Implement listing all courses logic
+  const courses = await Course.find({});
+
+  res.json({
+    response: courses,
+  });
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+router.post("/courses/:courseId", userMiddleware, async (req, res) => {
+  // Implement course purchase logic
+  const username = req.headers.username;
+  const courseId = req.params.courseId;
+
+  // const user = await User.findOne({
+  //   username,
+  // });
+
+  // console.log(user);
+
+  await User.updateOne(
+    { username: username },
+    {
+      $push: { purchasedCourses: courseId },
+    }
+  );
+
+  res.json({
+    message: "Course purchased successfully",
+  });
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
+router.get("/purchasedCourses", userMiddleware, async (req, res) => {
+  // Implement fetching purchased courses logic
+  const username = req.headers.username;
+  const user = await User.findOne({
+    username,
+  });
+
+  const course = await Course.find({
+    _id: {
+      $in: user.purchasedCourses,
+    },
+  });
+
+  res.json({
+    purchasedCourses: course,
+  });
 });
 
-module.exports = router
+module.exports = router;
